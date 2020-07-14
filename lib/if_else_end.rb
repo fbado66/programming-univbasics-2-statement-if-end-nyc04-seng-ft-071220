81105ffd543129fbd4fa520e124f91d1624f7fe3
Franklin Bado <fbado66@gmail.com>
2020-07-14 02:56:15 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,12 +1,12 @@
 # Write your solution here
 
-# current_second = Time.now.sec.to_i
-# puts "This is the current second #{current_second}"
-# if current_second % 2 == 0
-#   puts "Even!"
-# else 
-#   puts "Odd!"
-# end 
+current_second = Time.now.sec.to_i
+puts "This is the current second #{current_second}"
+if current_second % 2 == 0
+  puts "Even!"
+else 
+  puts "Odd!"
+end 
 
 # if Time.now.to_i % 2 == 0 
 #   puts "Even!"
